a83115eee6eee197d74ee6ee6765df4fa25dd5f6
Andrew Brown <andy_zero@hotmail.com>
2023-10-01 20:04:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css';          
+
+// function App() {             
+// return (                    
+//   <div>
+//     <h1 className="title">Hello World!</h1>
+//       <p> This is a paragraph of text</p>
+// </div>
+//   );
+// }  
 
 function App() {
+  
+  const name = "Andy";
+  const age = 36;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>My name is {name} and I'm {age} years old</p>
     </div>
   );
 }
